b27c6100fbe09d37ab21989b20716e5b3f29334c
Ulyzses <mariusulyzses.barcenas@gmail.com>
2020-01-13 13:29:20 +0800

    Added about page

---
+++ public_html/about/index.php
@@ -0,0 +1,37 @@
+<?php
+
+session_start();
+
+ ?>
+<?php include $_SERVER['DOCUMENT_ROOT'] . "/cdr/inc/begin.php" ?>
+  <link rel="stylesheet" href="../css/about.css">
+  <title>About | UPIS CDR</title>
+</head>
+<body>
+  <?php include $_SERVER['DOCUMENT_ROOT'] . "/cdr/inc/navbar.php" ?>
+  <div class="container">
+    <div class="row">
+      <div class="col-7 mx-auto my-5">
+        <h1 class="title">About</h1>
+        <p class="content">Pellentesque in ipsum id orci porta dapibus. Proin eget tortor risus. Proin eget tortor risus. Donec rutrum congue leo eget malesuada. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus.</p>
+        <h2 class="header">Computerised Data Registry</h2>
+        <p class="content">Quisque velit nisi, pretium ut lacinia in, elementum id enim. Pellentesque in ipsum id orci porta dapibus. Cras ultricies ligula sed magna dictum porta. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Curabitur aliquet quam id dui posuere blandit. Nulla quis lorem ut libero malesuada feugiat.</p>
+        <h2 class="header">Cramming Dauntless Researchers</h2>
+        <p class="content">Curabitur aliquet quam id dui posuere blandit. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Nulla quis lorem ut libero malesuada feugiat. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.</p>
+        <p class="content">Donec sollicitudin molestie malesuada. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Sed porttitor lectus nibh. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Curabitur aliquet quam id dui posuere blandit.</p>
+        <h2 class="header">Development</h2>
+        <p class="content">The Computerised Data Registry is developed by Marius Barcenas for their thesis. For inquiries regarding its development, you may reach him via +63 929 756 0504 or through his email at barcenasmarius@gmail.com.</p>
+        <h2 class="header">Disclaimer</h2>
+        <p class="content">The Computerised Data Registry is an online grade-viewing platform independently developed for the purposes of the thesis of Cramming Dauntless Researchers. Though currently deployed and used in UPIS, it is not an official website of the school, managed by the school, nor does it reflect the views of the school in any way, shape, or form.</p>
+        <p class="content">Users of this platform have full autonomy to publish data as is bound by the level of their authority. As such, in no event will Cramming Dauntless Researchers be liable for any false, incorrect, or sensitive information published to this website by its users.</p>
+        <h2 class="header">Quack</h2>
+        <p class="content">Since you actually visited the about page, look at this duck.</p>
+      </div>
+    </div>
+  </div>
+  <div class="end">
+    <a href="https://www.flickr.com/photos/27391161@N07/3282846730" target="_blank">Claudio Gennari / Flickr</a>
+  </div>
+</body>
+
+</html>
