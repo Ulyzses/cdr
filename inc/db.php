6c5cfe6e031cfe1b5a1413a2acfae52cfa3899f6
Ulyzses <mariusulyzses.barcenas@gmail.com>
2020-01-23 21:40:39 +0800

    Added backup localhost connection in case IP changes

--- inc/db.php
+++ inc/db.php
@@ -1,5 +1,24 @@
 <?php
 
+function db_connect_local() {
+  static $conn;
+  
+  if ( !isset($conn) ) {
+    // parse database variables from config.ini
+    $config = parse_ini_file( $_SERVER['DOCUMENT_ROOT'] . '/cdr/inc/config.ini');
+
+    // connect to database
+    $conn = mysqli_connect($config['oldhost'], $config['olduser'], $config['oldpass'], $config['oldname']);
+
+    if ( mysqli_connect_errno() ) {
+      die(mysqli_connect_error());
+    }
+  }
+
+  echo "Using local connection";
+  return $conn;
+}
+
 function db_connect() {
   static $conn;
 
@@ -11,7 +30,8 @@ function db_connect() {
     $conn = mysqli_connect($config['host'], $config['user'], $config['pass'], $config['name']);
 
     if ( mysqli_connect_errno() ) {
-      die(mysqli_connect_error());
+      // die(mysqli_connect_error());
+      $conn = db_connect_local();
     }
   }
 
