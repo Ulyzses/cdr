8b11fd625b494f0ea4049710abdcabfb7e265c8d
Ulyzses <mariusulyzses.barcenas@gmail.com>
2020-01-25 22:16:10 +0800

    Finished sidebar for profile

--- public_html/profile/index.php
+++ public_html/profile/index.php
@@ -8,6 +8,25 @@ if ( !(isset($_SESSION['logged_in']) && $_SESSION['logged_in'])) {
 
 require($_SERVER['DOCUMENT_ROOT'] . "/cdr/inc/db.php");
 
+// Get Image
+$query = "
+  SELECT `file_name`
+  FROM `images`
+  WHERE `user_code` = '{$_SESSION['user_code']}'
+";
+
+$result = mysqli_query($conn, $query);
+
+if ( $result ) {
+  if ( mysqli_num_rows($result) == 0 ) {
+    $displayImg = "null";
+  } else {
+    $displayImg = "../img/profile/" . mysqli_fetch_row($result)[0];
+  }
+} else {
+  die(mysqli_error($conn));
+}
+
  ?>
 <?php include $_SERVER['DOCUMENT_ROOT'] . "/cdr/inc/begin.php" ?>
   <link rel="stylesheet" href="/cdr/public_html/css/profile.css">
@@ -17,11 +36,48 @@ require($_SERVER['DOCUMENT_ROOT'] . "/cdr/inc/db.php");
 <body>
   <?php include $_SERVER['DOCUMENT_ROOT'] . "/cdr/inc/navbar.php" ?>
   <div class="container-fluid flex-grow-1">
-    <form action="upload.php" enctype="multipart/form-data" id="uploadPicForm">
-      Select Image File to Upload:
+    <div class="row justify-content-center h-100">
+      <div class="col-lg-2 col-md-3 col-12 side">
+        <!-- load image -->
+        <img src="<?php echo $displayImg ?>" alt="Display Photo" id="displayImg">
+
+        <h1 class="side-text side-header">Name</h1>
+        <h2 class="side-text side-details">
+          <?php echo "{$_SESSION['user_data']['first_name']} {$_SESSION['user_data']['last_name']}" ?>
+        </h2>
+
+        <h1 class="side-text side-header">Username</h1>
+        <h2 class="side-text side-details">
+          <?php echo "{$_SESSION['username']}" ?>
+        </h2>
+
+        <h1 class="side-text side-header">User Code</h1>
+        <h2 class="side-text side-details">
+          <?php echo "{$_SESSION['user_code']}" ?>
+        </h2>
+
+        <h1 class="side-text side-header">Account Type</h1>
+        <h2 class="side-text side-details">
+          <?php 
+            if ( $_SESSION['type'] === 0 ) {
+              echo "Administrator";
+            } else if ( $_SESSION['type'] === 1) {
+              echo "Teacher";
+            } else if ( $_SESSION['type'] === 2) {
+              echo "Student";
+            } else {
+              echo " What the fuk r u ";
+            }
+          ?>
+        </h2>
+      </div>
+      <div class="col-lg-7 col-md-7 col-12 main">
+      </div>
+    </div>
+    <form action="upload.php" enctype="multipart/form-data" id="uploadPicForm" class="d-none">
       <input type="hidden" name="request" value="upload">
-      <input type="file" class="form-control" name="file" id="file">
-      <input type="submit" class="btn btn-primary" name="submit" value="Upload">
+      <input type="file" class="form-control d-none" name="file" id="file">
+      <!-- <input type="submit" class="btn btn-primary" name="submit" value="Upload"> -->
     </form>
   </div>
 </body>
