e830548843fc6974d4db3d11328bc9f3f680f53f
Ulyzses <mariusulyzses.barcenas@gmail.com>
2020-02-04 23:49:08 +0800

    Added read notifications

--- public_html/bridge.php
+++ public_html/bridge.php
@@ -7,6 +7,17 @@ if ( isset($_POST['request']) ) {
     case 'get_notifications':
       require($_SERVER['DOCUMENT_ROOT'] . "/cdr/inc/notification.php");
       die(json_encode(getNotifications()));
+      break;
+    case 'read_notification':
+      require($_SERVER['DOCUMENT_ROOT'] . "/cdr/inc/notification.php");
+      readNotification($_POST['id']);
+      break;
+    case 'read_all':
+      require($_SERVER['DOCUMENT_ROOT'] . "/cdr/inc/notification.php");
+      readAllNotifications();
+      break;
+    default:
+      die("Unknown request");
   }
 }
 
