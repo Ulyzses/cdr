1198f8b7443a77b0b9f64811e4400abae24db6b2
Ulyzses <mariusulyzses.barcenas@gmail.com>
2020-03-05 22:05:41 +0800

    Corrected spelling for announcement

--- public_html/teacher/index.php
+++ public_html/teacher/index.php
@@ -95,7 +95,7 @@ if ( $result ) {
           <div class="modal-dialog" role="document">
             <div class="modal-content">
               <div class="modal-header">
-                <h5 class="modal-title">Create Annoucnement</h5>
+                <h5 class="modal-title">Create Announcement</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                   <span aria-hidden="true">&times;</span>
                 </button>
