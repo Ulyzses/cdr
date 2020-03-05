a898b48a68044db83cdf2916257a1fba2e79ec49
Ulyzses <mariusulyzses.barcenas@gmail.com>
2020-03-05 11:16:53 +0800

    Fixed mysqli_real_escape_string bug

--- inc/announcement.php
+++ inc/announcement.php
@@ -51,8 +51,8 @@ function createAnnouncement($details) {
 
   $classCode = $details['classCode'] ?? "";
   $scope = $details['scope'];
-  $title = mysqli_real_escape_string($details['title']);
-  $message = mysqli_real_escape_string($details['message']);
+  $title = mysqli_real_escape_string($conn, $details['title']);
+  $message = mysqli_real_escape_string($conn, $details['message']);
   $time = $details['time'] ?? time();
   $senderCode = $details['senderCode'] ?? $_SESSION['user_code'];
 
