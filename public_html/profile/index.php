151625befe0068b7d233ec2ec30121d9c708a065
Ulyzses <mariusulyzses.barcenas@gmail.com>
2020-03-08 18:25:24 +0800

    Added profile page details

--- public_html/profile/index.php
+++ public_html/profile/index.php
@@ -28,6 +28,60 @@ if ( $result ) {
   die(mysqli_error($conn));
 }
 
+mysqli_free_result($result);
+
+// For teachers
+if ( $_SESSION['type'] == 1 ) {
+  $query = "
+    SELECT
+      `class_subject` AS subject,
+      `class_code` AS code,
+      `class_level` AS level,
+      `class_section` AS section,
+      `class_room` AS room
+    FROM `classes`
+    WHERE `class_teacher` = '{$_SESSION['user_code']}'
+  ";
+
+  $result = mysqli_query($conn, $query);
+
+  if ( $result ) {
+    $classes = array();
+
+    while ($row = mysqli_fetch_assoc($result)) {
+      $classes[] = $row;
+    }
+  } else {
+    die(mysqli_error($conn));
+  }
+} else if ( $_SESSION['type'] == 2 ) {
+  $query = "
+    SELECT
+      `classes`.`class_subject` AS subject,
+      `classes`.`class_room` AS room,
+      `users`.`user_first_name` AS teacher_first,
+      `users`.`user_last_name` AS teacher_last
+    FROM `enrolments`
+    JOIN `classes`
+    ON `enrolments`.`subject_code` = `classes`.`class_code`
+    JOIN `users`
+    ON `classes`.`class_teacher` = `users`.`user_code`
+    WHERE `enrolments`.`student_code` = '{$_SESSION['user_code']}'
+  ";
+
+  $result = mysqli_query($conn, $query);
+
+  if ( $result ) {
+    $subjects = array();
+
+    while ($row = mysqli_fetch_assoc($result)) {
+      $subjects[] = $row;
+    }
+  } else {
+    die(mysqli_error($conn));
+  }
+}
+
  ?>
 <?php include $_SERVER['DOCUMENT_ROOT'] . "/cdr/inc/begin.php" ?>
   <link rel="stylesheet" href="/cdr/public_html/css/profile.css">
@@ -51,10 +105,10 @@ if ( $result ) {
         <h2 class="side-text side-details">
           <?php echo "{$_SESSION['username']}" ?>
         </h2>
-
-        <h1 class="side-text side-header">User Code</h1>
+        
+        <h1 class="side-text side-header">Email Address</h1>
         <h2 class="side-text side-details">
-          <?php echo "{$_SESSION['user_code']}" ?>
+          <?php echo $_SESSION['user_data']['email']; ?>
         </h2>
 
         <h1 class="side-text side-header">Account Type</h1>
@@ -67,10 +121,12 @@ if ( $result ) {
             } else if ( $_SESSION['type'] === 2) {
               echo "Student";
             } else {
-              echo " What the fuk r u ";
+              echo "Unknown";
             }
           ?>
         </h2>
+
+        <button class="btn btn-outline-secondary w-100" id="editButton">Edit Account Details</button>
       </div>
 
       <!-- Right Content -->
@@ -78,15 +134,34 @@ if ( $result ) {
         <?php if ( $_SESSION['type'] == 1 ) : ?>
           <h1 class="body-header">My Classes</h1>
 
-          <div class="body-div">
-            <h2 class="body-text body-div-header">
-              11-CV Del Rosario
-            </h2>
-            <h3 class="body-text body-div-details">
-              English
-            </h3>
-          </div>
+          <?php foreach($classes as $class) : ?>
+            <div class="body-div">
+              <h2 class="body-text body-div-header">
+                <?php echo "{$class['level']}&ndash;{$class['section']}"; ?>
+              </h2>
+              <h3 class="body-text body-div-details">
+                <span id="subject">
+                  <?php echo $class['subject']; ?>
+                </span> | <span>
+                  <?php echo $class['room']; ?>
+                </span>
+              </h3>
+            </div>
+          <?php endforeach; ?>
 
+        <?php elseif ( $_SESSION['type'] == 2 ) : ?>
+          <h1 class="body-header">My Subjects</h1>
+
+          <?php foreach($subjects as $subject) : ?>
+            <div class="body-div">
+              <h2 class="body-text body-div-header">
+                <?php echo $subject['subject']; ?>
+              </h2>
+              <h3 class="body-text body-div-details">
+                  <?php echo "{$subject['teacher_last']}, {$subject['teacher_first']}"; ?>
+              </h3>
+            </div>
+          <?php endforeach; ?>
         <?php endif; ?>
       </div>
     </div>
