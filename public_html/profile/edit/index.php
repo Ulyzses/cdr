638fcde7ad24afea6701ccce3f2b1b47fbc9c692
Ulyzses <mariusulyzses.barcenas@gmail.com>
2020-03-08 19:03:14 +0800

    Edit profile initial commit

---
+++ public_html/profile/edit/index.php
@@ -0,0 +1,18 @@
+<?php
+
+session_start();
+
+?>
+<?php include $_SERVER['DOCUMENT_ROOT'] . "/cdr/inc/begin.php" ?>
+  <link rel="stylesheet" href="/cdr/public_html/css/edit.css">
+  <script src="/cdr/public_html/js/edit.js"></script>
+  <title>Edit Profile | UPIS CDR</title>
+</head>
+<body>
+  <?php include $_SERVER['DOCUMENT_ROOT'] . "/cdr/inc/navbar.php" ?>
+  <div class="container-fluid flex-grow-1">
+
+  </div>
+</body>
+
+</html>
